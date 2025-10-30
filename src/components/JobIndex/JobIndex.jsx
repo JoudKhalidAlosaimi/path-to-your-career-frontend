b8e55ac20e4791a1051c3717924bb1f13cb29887
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-30 16:15:18 +0300

    added two function one for showing the state of the application using a button, then the ability to change to state using a dropdown

--- src/components/JobIndex/JobIndex.jsx
+++ src/components/JobIndex/JobIndex.jsx
@@ -8,6 +8,8 @@ function JobIndex() {
     // display all our jobs
 
     const [jobs,setJobs] = useState([])
+    const [application, setApplication] = useState({})
+
 
     async function getAllJobs(){
         const response = await axios.get('http://127.0.0.1:8000/api/jobs/')
@@ -15,8 +17,40 @@ function JobIndex() {
     }
 
     useEffect(() => {
-        getAllJobs()
-    }, [])
+            getAllJobs()
+        }, [])
+
+    async function handleApplication(jobId) {
+        const ApplicationData = {
+            job : jobId,
+            status : "Applied"
+        }
+        const response = await axios.post('http://127.0.0.1:8000/api/applications/', ApplicationData)
+        // https://stackoverflow.com/questions/55823296/reactjs-prevstate-in-the-new-usestate-react-hook
+        setApplication(prevState => ({
+            ...prevState,
+            [jobId] : {
+                status : response.data.status,
+                id : response.data.id
+            }
+        }))
+    }
+
+    async function handleApplicationStatusChange(e,jobId,applicationId) {
+        const ApplicationStatusUpdate = {
+            status : e.target.value
+        }
+        const response = await axios.put(`http://127.0.0.1:8000/api/applications/${applicationId}/`,ApplicationStatusUpdate)
+        setApplication(prevState => ({
+            ...prevState,
+            [jobId] : {
+                ...prevState[jobId],
+                status : response.data.status
+            }
+            
+        }))
+        console.log(response.data.id)
+    }
     return (
         <div>
             {
@@ -28,6 +62,21 @@ function JobIndex() {
                             <p>{job.company}</p>
                             <p>{job.description}</p>
                             <p>{job.status}</p>
+
+                            {
+                                application[job.id] 
+                                ?
+                                <>
+                                <p>{application[job.id].status}</p>
+                                <select onChange={(e) => {handleApplicationStatusChange(e,job.id,application[job.id].id)}} value={application[job.id].status}>
+                                    <option value="Applied">Applied</option>
+                                    <option value="Rejected">Rejected</option>
+                                    <option value="Interview">Interview</option>
+                                </select>
+                                </>
+                                :
+                                <button onClick={() => {handleApplication(job.id)}}>Mark As Applied</button>
+                            }
                         </div>
                         )
                     })
