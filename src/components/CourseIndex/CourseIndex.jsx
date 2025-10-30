4aa14cd6c43d1cbcf0a31b2c2f0892d963bd70cd
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-31 00:27:52 +0300

    created two functions that handels the application model, one for assigning a value to the status field in the application model using POST and the other to change the value of the status field using PUT

--- src/components/CourseIndex/CourseIndex.jsx
+++ src/components/CourseIndex/CourseIndex.jsx
@@ -8,6 +8,7 @@ function CourseIndex() {
     // display all the courses
 
     const [courses,setCourses] = useState([])
+    const [application, setApplication] = useState({})
 
     async function getAllCourses(){
         const response = await axios.get('http://127.0.0.1:8000/api/courses/')
@@ -17,6 +18,37 @@ function CourseIndex() {
     useEffect(() => {
         getAllCourses()
     }, [])
+
+    async function handleApplication(courseId) {
+        const ApplicationData = {
+            course : courseId,
+            status : "Applied"
+        }
+        const response = await axios.post('http://127.0.0.1:8000/api/applications/', ApplicationData)
+        console.log(response.data)
+        setApplication( prevStatus => ({
+            ...prevStatus,
+            [courseId] : {
+                id : response.data.id,
+                status : response.data.status
+            }
+        }))
+    }
+
+    async function handleApplicationStatusChange(e,courseId,applicationId) {
+        const ApplicationStatusUpdate = {
+            status : e.target.value
+        }
+        const response = await axios.put(`http://127.0.0.1:8000/api/applications/${applicationId}/`, ApplicationStatusUpdate)
+        console.log(response.data)
+        setApplication(prevStatus => ({
+            ...prevStatus,
+            [courseId] : {
+                ...courseId,
+                status : response.data.status
+            }
+        }))
+    }
     return (
         <div>
             {
@@ -28,6 +60,21 @@ function CourseIndex() {
                             <p>{course.provider}</p>
                             <p>{course.description}</p>
                             <p>{course.duration}</p>
+
+                            {
+                                application[course.id] ?
+                                <>
+                                <p>{application[course.id].status}</p>
+                                <select onChange={(e) => {handleApplicationStatusChange(e,course.id,application[course.id].id)}} value={application[course.id].status}>
+                                    <option value="Applied">Applied</option>
+                                    <option value="Rejected">Rejected</option>
+                                    <option value="Interview">Interview</option>
+                                </select>
+                                </>
+                                :
+                                <button onClick={() => {handleApplication(course.id)}}>Mark As Applied</button>
+                                
+                            }
                         </div>
                         )
                     })
