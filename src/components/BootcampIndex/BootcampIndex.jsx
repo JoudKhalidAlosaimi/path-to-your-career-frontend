1858cfed445d0a1a74b2fc2ecf568a124670ea21
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-31 03:01:12 +0300

    Added the application model to the BootcampIndex to add a status and change a status

--- src/components/BootcampIndex/BootcampIndex.jsx
+++ src/components/BootcampIndex/BootcampIndex.jsx
@@ -8,6 +8,7 @@ function BootcampIndex() {
     // display all the bootcamps using map
 
     const [bootcamps,setBootcamps] = useState([])
+    const [application, setApplication] = useState({})
 
     async function getAllBootcamps(){
         const response = await axios.get('http://127.0.0.1:8000/api/bootcamps/')
@@ -17,6 +18,37 @@ function BootcampIndex() {
     useEffect(() => {
         getAllBootcamps()
     }, [])
+
+    async function handleApplication(bootcampId) {
+        const ApplicationData = {
+            bootcamp : bootcampId,
+            status : "Applied"
+        }
+        const response = await axios.post('http://127.0.0.1:8000/api/applications/', ApplicationData)
+        setApplication(prevState => ({
+            ...prevState,
+            [bootcampId] : {
+                status : response.data.status,
+                id : response.data.id
+            }
+        }))
+    }
+
+    async function handleApplicationStatusChange(e,bootcampId,applicationId) {
+        const ApplicationStatusUpdate = {
+            status : e.target.value
+        }
+        const response = await axios.put(`http://127.0.0.1:8000/api/applications/${applicationId}/`,ApplicationStatusUpdate)
+        setApplication(prevState => ({
+            ...prevState,
+            [bootcampId] : {
+                ...prevState[bootcampId],
+                status : response.data.status
+            }
+            
+        }))
+        console.log(response.data.id)
+    }
     return (
         <div>
             {
@@ -29,6 +61,21 @@ function BootcampIndex() {
                             <p>{bootcamp.description}</p>
                             <p>{bootcamp.start_date}</p>
                             <p>{bootcamp.end_date}</p>
+
+                            {
+                                application[bootcamp.id] 
+                                ?
+                                <>
+                                <p>{application[bootcamp.id].status}</p>
+                                <select onChange={(e) => {handleApplicationStatusChange(e,bootcamp.id,application[bootcamp.id].id)}} value={application[bootcamp.id].status}>
+                                    <option value="Applied">Applied</option>
+                                    <option value="Rejected">Rejected</option>
+                                    <option value="Interview">Interview</option>
+                                </select>
+                                </>
+                                :
+                                <button onClick={() => {handleApplication(bootcamp.id)}}>Mark As Applied</button>
+                            }
                         </div>
                         )
                     })
