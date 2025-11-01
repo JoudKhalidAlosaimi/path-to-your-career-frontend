738f431bd5b72dc7923bf2ef5dc24af9ce8ebef8
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-11-01 21:47:15 +0300

    added some CSS to the NavBar(to be improved)

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,31 +4,40 @@ import LogOutButton from "../Auth/LogOutButton"
 function NavBar({ user, setUser }) {
     return (
         <nav>
-            <div className="navbar-leftside">
-                <h2>CareerPathify</h2>
-            </div>
-
-            <div className="navbar-center">
-                <Link to={'/home'}>Home</Link>
-                <Link to={'/jobs'}>Jobs</Link>
-                <Link to={'/courses'}>Courses</Link>
-                <Link to={'/bootcamps'}>Bootcamps</Link>
-            </div>
-            
-            <div className="navbar-rightside">
-                {
-                    user
-                    ?
-                    <>
-                        <Link to={'/profile'}>Account</Link>
-                        <LogOutButton setUser={setUser} />
-                    </>
-                    :
-                    <>
-                        <Link to={'/login'}>Login</Link>
-                        <Link to={'/register'}>Create an account</Link>
-                    </>
-                }
+            <div className="bg-gray-800 text-white fixed w-full z-50">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex flex-row w-full justify-between">
+                            <div className="text-xl font-bold">
+                                <h2>CareerPathify</h2>
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="flex ml-10 items-baseline space-x-2">
+                                    <Link to={'/home'}>Home</Link>
+                                    <Link to={'/jobs'}>Jobs</Link>
+                                    <Link to={'/courses'}>Courses</Link>
+                                    <Link to={'/bootcamps'}>Bootcamps</Link>
+                                </div>
+                            </div>
+                            <div className="flex ml-10 items-baseline space-x-2">
+                            {
+                                user
+                                ?
+                                <>
+                                    <Link to={'/profile'}>Account</Link>
+                                    <LogOutButton setUser={setUser} />
+                                </>
+                                :
+                                <>
+                                    <Link to={'/login'}>Login</Link>
+                                    <Link to={'/register'}>Create an account</Link>
+                                </>
+                            }
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </nav>
     )
