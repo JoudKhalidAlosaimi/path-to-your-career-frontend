32bf1479ce9dcf9142edbb78c1691a96482184fc
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-11-04 23:30:18 +0300

    added some CSS to bootcampIndex,BootcampSearch and fixed some rendering issues

--- src/components/BootcampIndex/BootcampIndex.jsx
+++ src/components/BootcampIndex/BootcampIndex.jsx
@@ -14,6 +14,7 @@ function BootcampIndex({user}) {
     const [application, setApplication] = useState({})
     const [bookmarked,setBookmarked] = useState({})
     const [errors,setErrors] = useState(null)
+    const [applied,setApplied] = useState({})
 
     async function getAllBootcamps(){
         try {
@@ -25,8 +26,24 @@ function BootcampIndex({user}) {
         }
     }
 
+    async function getAllApplications(){
+        const ApplicantData = {
+            owner : user.user_id
+        }
+        try {
+            const response = await authRequest({method: 'get', url: 'http://127.0.0.1:8000/api/applications/', data: ApplicantData})
+            const appliedObject = {};
+            response.data.forEach(applied => {
+                    appliedObject[applied.bootcamp] = { id: applied.id, status: applied.status }})
+            setApplied(appliedObject)
+        } catch(error) {
+            setErrors(error.response.data.error)
+        }
+    }
+
     useEffect(() => {
-        getAllBootcamps()
+        getAllBootcamps(),
+        getAllApplications()
     }, [])
 
     const searchBootcamps = (searchInput) => {
@@ -55,6 +72,13 @@ function BootcampIndex({user}) {
                     id : response.data.id
                 }
             }))
+            setApplied(prevState => ({
+                ...prevState,
+                [bootcampId] : {
+                    status : response.data.status,
+                    id : response.data.id
+                }
+            }))
         } catch(error) {
             setErrors(error.response.data.error)
         }
@@ -73,7 +97,13 @@ function BootcampIndex({user}) {
                     ...prevState[bootcampId],
                     status : response.data.status
                 }
-                
+            }))
+            setApplied(prevState => ({
+                ...prevState,
+                [bootcampId] : {
+                    ...prevState[bootcampId],
+                    status : response.data.status
+                }
             }))
         } catch(error) {
             setErrors(error.response.data.error)
@@ -109,65 +139,63 @@ function BootcampIndex({user}) {
                 setErrors(error.response.data.error)
             }
     }
-    return (
-        <div className="min-h-screen p-8 pt-30">
+return (
+    <div className="min-h-screen p-8 pt-30 mb-60">
+        <div className="max-w-7xl mx-auto">
             <h1 className="text-3xl font-bold text-center mb-8">Available Bootcamps</h1>
-            <h2 className="text-3xl font-bold text-center mb-8">Search</h2>
             <BootcampSearch searchBootcamps={searchBootcamps} displayedBootcamps={displayedBootcamps} reset={reset}/>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
                 {
-                    displayedBootcamps.length ?
+                    displayedBootcamps ?
                     displayedBootcamps.map((bootcamp,index) => {
                         return (
-                            <div key={index} className="bg-white shadow-md rounded-xl p-6">
+                            <div key={index} className="relative bg-white rounded-2xl p-6 shadow-lg hover:-translate-y-3">
+                                <button
+                                onClick={() => handleBookmark(bookmarked[bootcamp.id]?.id, bootcamp.id)}
+                                className="absolute top-4 right-4 w-10 h-10 flex items-center justify-center bg-gray-100 text-blue-600 rounded-xl">
+                                    {bookmarked[bootcamp.id]?.value ? 
+                                        <i className="fa-solid fa-bookmark"></i>
+                                        : 
+                                        <i className="fa-regular fa-bookmark"></i>
+                                    }
+                                </button>
                                 <h2 className="text-xl font-semibold text-gray-800 mb-2">{bootcamp.title}</h2>
                                 <p className="text-gray-600 mb-1"><span className="font-medium">Company:</span> {bootcamp.provider}</p>
                                 <p className="text-gray-500 text-sm mb-4">{bootcamp.description}</p>
                                 <p className="text-gray-500 text-sm mb-4">Starts at : {bootcamp.start_date}, ends at :{bootcamp.end_date}</p>
-
                                 {
-                                    application[bootcamp.id] 
+                                    applied[bootcamp.id] 
                                     ?
                                     <div className="mt-4">
                                         <p className="text-sm font-medium text-gray-700 mb-2">
-                                            Status: <span className="text-blue-600">{application[bootcamp.id].status}</span>
+                                            Status: <span className="text-blue-500 font-semibold">{applied[bootcamp.id].status}</span>
                                         </p>
                                         <select 
-                                        onChange={(e) => {handleApplicationStatusChange(e,bootcamp.id,application[bootcamp.id].id)}} 
-                                        value={application[bootcamp.id].status}
-                                        className="w-full border border-gray-300 rounded-md p-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                        onChange={(e) => {handleApplicationStatusChange(e,bootcamp.id,applied[bootcamp.id].id)}} 
+                                        value={applied[bootcamp.id].status}
+                                        className="w-full border border-gray-800 rounded-md p-2 text-sm text-gray-800 bg-blue-200">
                                             <option value="Applied">Applied</option>
                                             <option value="Rejected">Rejected</option>
                                             <option value="Interview">Interview</option>
                                         </select>
-                                    </div>
-                                    :
-                                    <>
-                                    <button 
-                                    onClick={() => {handleApplication(bootcamp.id)}}
-                                    className="w-full bg-blue-600 text-white font-medium rounded-md p-2 mt-4 hover:bg-blue-700 transition">
-                                        Mark As Applied
-                                    </button>
-                                    <button
-                                    onClick={() => handleBookmark(bookmarked[bootcamp.id]?.id, bootcamp.id)}
-                                    className="w-full bg-blue-600 text-white font-medium rounded-md p-2 mt-4 hover:bg-blue-700 transition">
-                                        {bookmarked[bootcamp.id]?.value  ? (
-                                            <i className="fa-solid fa-bookmark"></i>
-                                        ) : (
-                                            <i className="fa-regular fa-bookmark"></i>
-                                        )}
-                                    </button>
-                                    </>
-                                }
+                                        </div>
+                                        :
+                                        <button 
+                                        onClick={() => {handleApplication(bootcamp.id)}}
+                                        className="w-full bg-blue-700 text-white font-medium rounded-md p-2 mt-4 hover:bg-blue-700">
+                                            Mark As Applied
+                                        </button>
+                                    }
                             </div>
                             )
                         })
                         :
                         <h1>Sorry, there are no bootcamps yet ...</h1>
                 }
+            </div>
         </div>
     </div>
-    )
+)
 }
 
 export default BootcampIndex
